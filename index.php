785469fe9cfde906754adf2084bed835e7f7f670
tmanley1985 <tmanley1985@gmail.com>
2014-10-15 22:05:50 -0400

    Added the header and nav elements

--- index.php
+++ index.php
@@ -1,8 +1,29 @@
 <!DOCTYPE>
 <html>
 <head>
+		<meta charset="UTF-8">
+			<link rel="stylesheet" type="text/css" href="css/normalize.css">
+			<link rel="stylesheet" type="text/css" href="css/grid.css">
+			<link rel="stylesheet" type="text/css" href="css/main.css">
+			<link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+			<link href='http://fonts.googleapis.com/css?family=Shadows+Into+Light' rel='stylesheet' type='text/css'>
+			<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js" ></script>
+			<script src="js/responsiveslides/responsiveslides.min.js"></script>
 </head>
 <body>
+	<div id="wrapper">
+		<header>
+			<h1></h1>
+			<nav>
+				<ul>
+					<li><a href="">Home</a></li>
+					<li><a href="">Accolades</a></li>
+					<li><a href="">Media</a></li>
+					<li><a href="">Sponsorship</a></li>
+				</ul>
+			</nav>
+		</header>
+	</div>
 
 </body>
 </html>
