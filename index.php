58334477ec342d7bd87608d8d2c113a195d0423a
tmanley1985 <tmanley1985@gmail.com>
2014-11-25 19:14:54 -0500

    Added variable to act as notifier of current page

--- index.php
+++ index.php
@@ -1,45 +1,19 @@
-<!DOCTYPE>
-<html>
-<head>
-			<meta charset="UTF-8">
-			<title>Throwdown Boxing Club</title>
-			<link rel="stylesheet" type="text/css" href="css/normalize.css">
-			<link rel="stylesheet" type="text/css" href="css/simplegrid.css">
-			<link rel="stylesheet" type="text/css" href="css/main.css">
-			<link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
-			<link href='http://fonts.googleapis.com/css?family=Shadows+Into+Light' rel='stylesheet' type='text/css'>
-			<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js" ></script>
-			<script src="js/responsiveslides/responsiveslides.min.js"></script>
-</head>
-<body>
-	<div id="wrapper"> 
-		<img src="graphics/logo.png">
-		<div class="grid grid-pad">
-			<div class="col-1-1">
-				<h1>Throwdown Boxing Club</h1>
+<?php
+	$page = 'home';
+	require('inc/header.php');
+
+?>
+		
+			<div class="rslides_container">
+				<ul class="rslides" id="slider1">
+					<li>
+						<iframe class="youtube-player" id="vid" type="text/html" width="450" height="500" src="http://www.youtube.com/embed/bMEFF-w-BtQ"> </iframe>
+					</li>
+					<li><img src="graphics/alifrazier.jpg" alt="Ali and Frazier"></li>
+					<li><img src="graphics/handwraps.jpg" alt="Handwraps"></li>
+					<li><img src="graphics/ammyboxing.jpg" alt="Ammy Boxing"></li>
+				</ul>
 			</div>
-		</div>
-		<header>
-			<div class="grid grid-pad">
-				<div class="col-1-1">
-					<nav>
-						<ul>
-							<li class="selected"><a href="">Home</a></li>
-							<li><a href="">Accolades</a></li>
-							<li><a href="">Media</a></li>
-							<li><a href="">Sponsorship</a></li>
-						</ul>
-					</nav>
-				</div>
-			</div>
-		</header>
-		<div class="grid-pad">
-			<ul class="rslides">
-				<li><img src="graphics/alifrazier.jpg" alt="Ali and Frazier"></li>
-				<li><img src="graphics/handwraps.jpg" alt="Handwraps"></li>
-				<li><img src="graphics/ammyboxing.jpg" alt="Ammy Boxing"></li>
-			</ul>
-		</div>
 
 		<div class="grid grid-pad">
 			<div class="center">
@@ -48,10 +22,12 @@
 						<h2>About Us</h2>
 						<br>
 						<p>Throwdown Boxing Club is Louisville
-							KY's premier boxing gym.  With
-							numerous amateur champions, golden
-							and silver gloves winners Throwdown
-							has cemented itself as the place
+							KY's premier boxing gym.  A 501(c)3 
+							nonprofit organization founded by 
+							Coach Steve Bailey, with numerous 
+							amateur champions, golden and silver 
+							gloves winners Throwdown has
+							cemented itself as the place
 							to be for boxing in Louisville.</p>
 					</section>
 				</div>
@@ -77,12 +53,7 @@
 				</div>
 			</div>
 		</div>
-	</div>
-<script>
-	  $(function() {
-	    $(".rslides").responsiveSlides();
-	  });
-</script>
-</body>
-</html>
 
+<?php
+	require('inc/footer.php');
+?>
