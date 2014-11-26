9dbb51b2fca3c6ee651956b20775fb36f781a72e
tmanley1985 <tmanley1985@gmail.com>
2014-11-25 22:38:01 -0500

    Added new cover picture to image slider

--- facility.php
+++ facility.php
@@ -5,30 +5,18 @@
 ?>
 	<div class="rslides_container">
 		<ul class="rslides" id="slider1">
+			<li><img src="graphics/facility.png"><p class="caption">Fully equipped facility!</p></li>
 			<li><img src="graphics/ring1.jpg"><p class="caption">Our 24ft. Ring!</p></li>
-			<li><img src="graphics/ring2.jpg"><p class="caption">Another view</p></li>
+			<li><img src="graphics/ring2.jpg"><p class="caption">Coach Steve conditioning our athletes</p></li>
 			<li><img src="graphics/cage.jpg"><p class="caption">Our 20ft. cage!</p></li>
 			<li><img src="graphics/trentjeff.jpg"><p class="caption">Trent and Jeff warming up</p></li>
 			<li><img src="graphics/smallring.jpg"><p class="caption">Our 16ft. ring</p></li>
+			<li><img src="graphics/alex.jpg"><p class="caption">Coach Alex demonstrating technique</p></li>
+
 
 		</ul>
 	</div>
 
-		<div class="center">
-			<h1>Facility</h1>
-			<p>The Sylvania Community Center is home to Throwdown Boxing Club, a full sized facility nestled in the PRP neighborhood of Louisville, KY.  Our facility is equipped with:
-			</p>
-			<ul>
-				<li>22 Ft. Competition Boxing Ring</li>
-				<li>16 ft. Floor Ring</li>
-				<li>20 ft. Cage</li>
-				<li>Free Weights</li>
-				<li>10 Heavy Bags</li>
-				<li>5 Double End Bags</li>
-				<li>2 Speed Bag Platforms</li>
-				<li>1000 sq ft. room completely padded from wall to floor</li>
-			</ul>
-		</div>
 
 <?php
 	require('inc/footer.php');
