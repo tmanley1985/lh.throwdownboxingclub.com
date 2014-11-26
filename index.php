ccf089900d3ef8599674cd77ebd839a86175d519
tmanley1985 <tmanley1985@gmail.com>
2014-11-25 22:39:20 -0500

    Added some pictures and added captions to all pictures in image slider

--- index.php
+++ index.php
@@ -9,9 +9,9 @@
 					<li>
 						<iframe class="youtube-player" id="vid" type="text/html" width="450" height="500" src="http://www.youtube.com/embed/bMEFF-w-BtQ"> </iframe>
 					</li>
-					<li><img src="graphics/alifrazier.jpg" alt="Ali and Frazier"></li>
-					<li><img src="graphics/handwraps.jpg" alt="Handwraps"></li>
-					<li><img src="graphics/ammyboxing.jpg" alt="Ammy Boxing"></li>
+					<li><img src="graphics/awards.jpg" alt="awards"><p class="caption">Silver Gloves, Golden Gloves and National Champions!</p></li>
+					<li><img src="graphics/handwraps.jpg" alt="Handwraps"><p class="caption">Hard work and dedication</p></li>
+					<li><img src="graphics/ammyboxing.jpg" alt="Ammy Boxing"><p class="caption">REAL Boxing in Louisville</p></li>
 				</ul>
 			</div>
 
@@ -22,13 +22,16 @@
 						<h2>About Us</h2>
 						<br>
 						<p>Throwdown Boxing Club is Louisville
-							KY's premier boxing gym.  A 501(c)3 
+							KY's premier boxing gym; a 501(c)3 
 							nonprofit organization founded by 
-							Coach Steve Bailey, with numerous 
+							Head Coach Steve Bailey. Assisted 
+							by Coach Thomas A. Manley and Rock
+							Cruz, our gym has built numerous 
 							amateur champions, golden and silver 
-							gloves winners Throwdown has
-							cemented itself as the place
-							to be for boxing in Louisville.</p>
+							gloves winners. Throwdown has
+							cemented itself as the only place
+							to be for REAL boxing in Louisville.
+							</p>
 					</section>
 				</div>
 
@@ -48,7 +51,12 @@
 					<section>
 						<h2>News</h2>
 						<br>
-						<p>5 Bobby Hughes National Champions!</p>
+						<p>Headed to the Indiana Silver Gloves!
+							The whole team is gearing up for some
+							stiff competition.  The Indiana Silver
+							Gloves are a Regional tournament with
+							opportunities to advance to the National
+							level.</p>
 					</section>
 				</div>
 			</div>
