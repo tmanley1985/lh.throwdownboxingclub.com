0354deaa1b537d9a6140fbfc794656d074d6c950
tmanley1985 <tmanley1985@gmail.com>
2014-11-25 19:17:45 -0500

    Added info page with google maps

---
+++ info.php
@@ -0,0 +1,30 @@
+<?php
+	$page = 'info';
+	require('inc/header.php');
+
+?>
+<div class="center">
+	<div class="grid grid-pad">
+	
+		<div class="info col-1-1">
+			<h2>Throwdown Boxing Club</h2>
+			<h3>6650 Sylvania 5 Rd, Louisville, KY 40258</h3>
+			<h3>Phone Number:</h3>
+			<h3>Head Coach: Steve Bailey</h3>
+			<h3>Assistant Coach: Thomas A. Manley</h3>
+			<h4>Directions</h4>
+
+			<div id="map_canvas">
+				<!-- Contains Google Map -->
+			</div>
+		</div>
+		<div class="col-2-3">
+			
+		</div>
+	</div>
+</div>
+
+
+<?php
+	require('inc/footer.php');
+?>
