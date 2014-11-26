54452a81a4cd314b6e8da8199b791a37f699112e
tmanley1985 <tmanley1985@gmail.com>
2014-11-25 22:39:57 -0500

    Added contact information

--- info.php
+++ info.php
@@ -8,11 +8,12 @@
 	
 		<div class="info col-1-1">
 			<h2>Throwdown Boxing Club</h2>
+			<br>
 			<h3>6650 Sylvania 5 Rd, Louisville, KY 40258</h3>
-			<h3>Phone Number:</h3>
+			<h3>Phone Number:(502)664-7304</h3>
 			<h3>Head Coach: Steve Bailey</h3>
 			<h3>Assistant Coach: Thomas A. Manley</h3>
-			<h4>Directions</h4>
+			<h3>Directions</h3>
 
 			<div id="map_canvas">
 				<!-- Contains Google Map -->
